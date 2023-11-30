1574c6d128049e426be70bd39f7b0e9181baf0fc
Wesley1808 <74668381+Wesley1808@users.noreply.github.com>
2023-11-30 17:59:19 +0100

    Update: 1.20.3-rc1

--- src/main/java/me/wesley1808/advancedchat/impl/predicates/CustomDistancePredicate.java
+++ src/main/java/me/wesley1808/advancedchat/impl/predicates/CustomDistancePredicate.java
@@ -1,8 +1,6 @@
 package me.wesley1808.advancedchat.impl.predicates;
 
-import com.google.gson.JsonElement;
-import com.mojang.datafixers.util.Pair;
-import com.mojang.serialization.*;
+import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import eu.pb4.predicate.api.PredicateResult;
 import me.wesley1808.advancedchat.api.AbstractChatPredicate;
@@ -11,12 +9,10 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.phys.Vec3;
 
-import java.util.Optional;
-
 public class CustomDistancePredicate extends AbstractChatPredicate {
     public static final ResourceLocation ID = new ResourceLocation("advancedchat", "distance");
     public static final MapCodec<CustomDistancePredicate> CODEC = RecordCodecBuilder.mapCodec(instance -> instance
-            .group(DistancePredicateCodec.INSTANCE.fieldOf("value").forGetter((inst) -> inst.predicate))
+            .group(DistancePredicate.CODEC.fieldOf("value").forGetter((inst) -> inst.predicate))
             .apply(instance, CustomDistancePredicate::new)
     );
 
@@ -32,26 +28,5 @@ public class CustomDistancePredicate extends AbstractChatPredicate {
         Vec3 pos1 = sender.position();
         Vec3 pos2 = target.position();
         return PredicateResult.ofBoolean(sender.level() == target.level() && this.predicate.matches(pos1.x, pos1.y, pos1.z, pos2.x, pos2.y, pos2.z));
-    }
-
-    private static class DistancePredicateCodec implements Codec<DistancePredicate> {
-        private static final DistancePredicateCodec INSTANCE = new DistancePredicateCodec();
-
-        @Override
-        public <T> DataResult<Pair<DistancePredicate, T>> decode(DynamicOps<T> ops, T input) {
-            Optional<DistancePredicate> optional = DistancePredicate.fromJson(input instanceof JsonElement jsonElement ? jsonElement : ops.convertTo(JsonOps.INSTANCE, input));
-            if (optional.isPresent()) {
-                return DataResult.success(new Pair<>(optional.get(), input));
-            } else {
-                return DataResult.error(() -> "Distance predicate JSON object is null");
-            }
-        }
-
-        @Override
-        public <T> DataResult<T> encode(DistancePredicate input, DynamicOps<T> ops, T prefix) {
-            // noinspection unchecked
-            T encoded = ops instanceof JsonOps ? (T) input.serializeToJson() : JsonOps.INSTANCE.convertTo(ops, input.serializeToJson());
-            return ops.getMap(encoded).flatMap(map -> ops.mergeToMap(prefix, map));
-        }
     }
 }
